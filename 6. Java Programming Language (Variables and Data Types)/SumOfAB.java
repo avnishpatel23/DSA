2fc85ec595029adabe6d72afe4967e2aedd01cd6
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-28 01:00:40 +0530

    Sum of A & B

---
+++ 6. Java Programming Language (Variables and Data Types)/SumOfAB.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SumOfAB{
+    public static void main(String args[]){
+         //code to calculate sum
+      /*   int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.print(sum);
+     */
+
+     //Sum of a&b (Input from user)
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    
+}
