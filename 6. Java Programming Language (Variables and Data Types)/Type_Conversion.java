3b3107460bdab9a576fd91cb1f679034cf205444
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-29 00:17:22 +0530

    Type Conversion

---
+++ 6. Java Programming Language (Variables and Data Types)/Type_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Type_Conversion{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        /*  int number = sc.nextFloat();
+            float number = sc.nextInt();
+            System.out.println(number);
+        */
+        float number = sc.nextInt();
+        System.out.println(number);
+    }
+}    
