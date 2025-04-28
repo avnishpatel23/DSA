35c7ca977276231fd17d367779fa2210cbd71315
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-29 00:17:51 +0530

    Type Promotion

---
+++ 6. Java Programming Language (Variables and Data Types)/Type_Promotion_in_Java.java
@@ -0,0 +1,9 @@
+
+public class Type_Promotion_in_Java{
+    public static void main(String args[]){
+       char a = 'b';
+       char b = 'b';
+       System.out.println(a);
+       System.out.println(b-a);
+    }
+}    
