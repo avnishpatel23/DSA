640818bf8ccc17f597c4e6934c6ba15f09b85124
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-29 00:18:19 +0530

    Variables and Data Types

---
+++ 6. Java Programming Language (Variables and Data Types)/VariablesAndDataTypes.java
@@ -0,0 +1,40 @@
+ng args[]public class VariablesAndDataTypes {
+    public static void main(Stri){
+        System.out.println("Hello World"); // ln = line ka space
+        System.out.print("Hello World\n");  // \n = for next line
+        System.out.println("Hello World"); // line ka space
+        System.out.println("Hello World\nHello \n World"); // line ka space
+
+        // Print Pattern
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        //Variables in Java
+        int a = 10;
+        int b = 5;
+        System.out.println("a"); // this print a not its value so
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Tony";
+        System.out.println(name);
+
+        a=50;
+        System.out.println(a);
+
+        //Data Types in Java
+        byte c = 8;
+        System.out.println(c);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        System.out.println(var);
+        int number = 23;
+        System.out.println(number);
+        //long
+        //double
+        short n = 240;
+        System.out.println(n);
+    }
+}
