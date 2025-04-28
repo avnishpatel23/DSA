b55d66e78b84526aac3e410c79aedcb165e06d82
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-29 00:16:55 +0530

    Type Casting

---
+++ 6. Java Programming Language (Variables and Data Types)/Type_Casting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Type_Casting{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float a = 25.12f;
+        // int b = a;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}    
