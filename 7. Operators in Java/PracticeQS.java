5ecc8a70ce8ca18a22600376e2d90b3c25ac5cb4
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-30 08:23:50 +0530

    Practice Ques

---
+++ 7. Operators in Java/PracticeQS.java
@@ -0,0 +1,157 @@
+// Ques 1
+
+/* 
+ public class PracticeQS {
+    public static void main(String[] args){
+    int x = 2, y = 5;
+    int exp1 = (x * y / x);
+    int exp2 = (x * (y / x));
+    System.out.print(exp1 + “ , ”);
+    System.out.print(exp2);
+    }
+*/
+
+
+// Ans 1
+
+/*
+ public class PracticeQS {
+    public static void main(String[] args){
+    int x = 2;
+    int y = 5;
+    int exp1 = (x * y / x);
+    int exp2 = (x * (y / x));
+    System.out.println(exp1);
+    System.out.println(exp2);
+    }
+ }
+*/
+
+
+// Ques 2
+
+/*
+ public class PracticeQS {
+    public static void main(String[] args) {
+    int x = 200, y = 50, z = 100;
+    if(x > y && y > z){
+    System.out.println("Hello");
+    }
+    if(z > y && z < x){
+    System.out.println("Java");
+    }
+    if((y+200) < x && (y+150) < z){
+    System.out.println("Hello Java");
+   }
+  }
+ }
+*/
+
+// Ans 2
+
+/*
+ public class PracticeQS {
+    public static void main(String[] args) {
+    int x = 200;
+    int y = 50;
+    int z = 100;
+    if(x > y && y > z){
+      System.out.println("Hello");
+    }
+    if(z > y && z < x){
+       System.out.println("Java");
+    }
+    if((y+200) < x && (y+150) < z){
+      System.out.println("Hello Java");
+   }
+ }
+ }
+*/
+
+   // Ques 3 
+
+/*
+ public class PracticeQS {
+   public static void main(String[] args){
+   int x, y, z;
+   x = y = z = 2;
+   x += y;
+   y -= z;
+   z /= (x + y);
+   System.out.println(x + " " + y + " " + z);
+   }
+ }
+*/
+
+// Ans 3
+
+/*
+ public class PracticeQS {
+   public static void main(String[] args){
+   int x;
+   int y;
+   int z;
+   x = y = z = 2;
+   x += y;
+   y -= z;
+   z /= (x + y);
+   System.out.println(x + " " + y + " " + z);
+   }
+ } 
+*/
+
+// Ques 4
+
+/*
+public class PracticeQS {
+   public static void main(String[] args){
+   int x = 9, y = 12;
+   int a = 2, b = 4, c = 6;
+   int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+   System.out.println(exp);
+   }
+}
+*/
+
+// Ans 4
+
+/*
+public class PracticeQS {
+   public static void main(String[] args){
+   int x = 9;
+   int y = 12;
+   int a = 2;
+   int b = 4;
+   int c = 6;
+   int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+   System.out.println(exp);
+   }
+}
+*/
+
+// Ques 5
+
+/*
+ public class PracticeQS {
+   public static void main(String[] args){
+   int x = 10, y = 5;
+   int exp1 = (y * (x / y + x / y));
+   int exp2 = (y * x / y + y * x / y);
+   System.out.println(exp1);
+   System.out.println(exp2);
+   }
+ }
+*/
+
+// Ans 5
+
+public class PracticeQS {
+   public static void main(String[] args){
+   int x = 10;
+   int y = 5;
+   int exp1 = (y * (x / y + x / y));
+   int exp2 = (y * x / y + y * x / y);
+   System.out.println(exp1);
+   System.out.println(exp2);
+   }
+}
