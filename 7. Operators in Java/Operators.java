430a92fc55951a6c5fae96ef2323d77f9aa74819
Avnish Patel <er.avnishpatel023@gmail.com>
2025-04-30 08:23:18 +0530

    Create Operators.java
    Operators in java

---
+++ 7. Operators in Java/Operators.java
@@ -0,0 +1,66 @@
+
+public class Operators{
+    public static void main(String args[]){
+      // Binary (Arithmatic Operator)
+
+      //  int A = 10;
+      //  int B = 5;
+      //  System.out.println("add = " +(A+B));
+      //  System.out.println("subtract = " +(A-B));
+      //  System.out.println("multiply = " +(A*B));
+      //  System.out.println("divide = " +(A/B));
+      //  System.out.println("modulo = " +(A%B));
+
+        // Unary Arithmatic Operator (Pre increment ++a )
+        
+        //  int a = 10;
+        //  int b = ++a;
+        //  System.out.println(a);
+        //  System.out.println(b);
+
+        // Unary Arithmatic Operator (Post increment a++ )
+
+        // int a = 10;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Unary Arithmatic Operator (Pre decrement --a )
+        
+          // int a = 10;
+          // int b = --a;
+          // System.out.println(a);
+          // System.out.println(b);
+
+          // Unary Arithmatic Operator (Post decrement a-- )
+        
+          // int a = 10;
+          // int b = a--;
+          // System.out.println(a);
+          // System.out.println(b);
+
+          // Relational Opertators
+
+         //  int A = 10;
+         //  int B = 5;
+         //  System.out.println(A==B);
+         //  System.out.println(A!=B);
+         //  System.out.println(A>B);
+         //  System.out.println(A<B);
+         //  System.out.println(A>=B);
+         //  System.out.println(A<=B);
+
+         // Logical Operators
+
+         // System.out.println((3>2) && (5>0));
+         // System.out.println((3>5) || (5>0));
+         // System.out.println(!(3>2));
+
+         // Assignment Operator
+         int A = 10;
+         // int B = 5;
+         A = A + 10;
+         //System.out.println(A = B);
+         System.out.println(A);
+    }
+}
