a6d648af00f0dd2b4e2afacf9a6693cb808955d9
Avnish Patel <er.avnishpatel023@gmail.com>
2025-05-01 09:03:50 +0530

    if else

---
+++ 8. Conditional Statements/if_else.java
@@ -0,0 +1,54 @@
+/*
+
+public class if_else {
+public static void main(String[] args) {
+    int age = 23;
+    if(age>18){
+       System.out.println("adult");
+    }
+    else{
+        System.out.println("Not Adult");
+    }   
+  }
+}
+
+*/
+
+// Print the largest of two numbewr
+
+
+/*
+import java.util.*;
+
+public class if_else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a>b){
+          System.out.println("a");
+        }
+        else {
+            System.out.println("b");
+        }
+    }
+}    
+
+*/
+
+// Print if a number is odd or even
+
+import java.util.*;
+
+public class if_else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num % 2 == 0){
+          System.out.println("EVEN");
+        }
+        else {
+            System.out.println("ODD");
+        }
+    }
+}    
