6cf6ee781e35e68d1c7fbe4b9062ebe15e04da38
Avnish Patel <er.avnishpatel023@gmail.com>
2025-05-02 09:41:23 +0530

    Continue

---
+++ 9. Loops/Continue.java
@@ -0,0 +1,31 @@
+/* 
+
+public class Continue{
+    public static void main(String[] args) {
+       for(int i=1; i<=5; i++){
+        if(i==3){
+            continue;
+        }
+        System.out.println(i);
+       }
+    }
+}
+
+*/
+
+// Display all numbers enteres by user except multiples of 10
+import java.util.*;
+public class Continue{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        do { 
+            System.out.println(" enter your number : ");
+            int n = sc.nextInt();
+
+            if(n % 10 == 0) {
+             continue;
+            }
+            System.out.println("number was :" + n);
+        } while(true);
+    }
+}
